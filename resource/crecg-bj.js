2c837fc1027a8a2d4b79d31c071d6fb6b2491c5e
yanxin <332622154@qq.com>
2022-08-17 02:10:13 +0800

    1.0

---
+++ resource/crecg-bj.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: yanxin 332622154@qq.com
+ * @Date: 2022-08-17 01:09:41
+ * @LastEditors: yanxin 332622154@qq.com
+ * @LastEditTime: 2022-08-17 01:56:24
+ * @FilePath: /crceg-bj.github.io/resource/crecg-bj.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+var crecgbjArr = [{
+    "name":   "第一建设公司",
+    "QRcode": "img/crecg-bj/1.crecg-bj.jpg",
+    "icon":   "img/crecg-bj/1.crecg-bj-icon.jpg"
+}, {
+    "name":   "京彩范儿",
+    "QRcode": "img/crecg-bj/2.jingcaifaner.jpeg",
+    "icon":   "img/crecg-bj/2.jingcaifaner-icon.jpeg"
+}, {
+    "name":   "京彩HR",
+    "QRcode": "img/crecg-bj/3.jingcaiHR.jpg",
+    "icon":   "img/crecg-bj/3.jingcaiHR-icon.jpg"
+}, {
+    "name":   "京彩学习中心",
+    "QRcode": "img/crecg-bj/4.jingcailearning.jpg",
+    "icon":   "img/crecg-bj/4.jingcailearning-icon.jpg"
+}, {
+    "name":   "廉洁京分",
+    "QRcode": "img/crecg-bj/5.lianjiejingfen.jpg",
+    "icon":   "img/crecg-bj/5.lianjiejingfen-icon.jpg"
+}
+]
---
+++ resource/subsidiaries.js
@@ -0,0 +1,49 @@
+/*
+ * @Author: yanxin 332622154@qq.com
+ * @Date: 2022-08-17 00:07:54
+ * @LastEditors: yanxin 332622154@qq.com
+ * @LastEditTime: 2022-08-17 01:40:28
+ * @FilePath: /crceg-bj.github.io/resource/subsidiaries.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+var subArr = [{
+    "name":   "副中心枢纽",
+    "QRcode": "img/subsidiaries/1.fzxsn.jpg"
+}, {
+    "name":   "副中心图书馆",
+    "QRcode": "img/subsidiaries/2.fzxtsg.jpg"
+}, {
+    "name":   "兰州机场",
+    "QRcode": "img/subsidiaries/3.lzjc.jpg"
+}, {
+    "name":   "长沙机场",
+    "QRcode": "img/subsidiaries/4.csjc.jpg"
+}, {
+    "name":   "长沙西站",
+    "QRcode": "img/subsidiaries/5.csxz.jpg"
+}, {
+    "name":   "容东E组团",
+    "QRcode": "img/subsidiaries/6.rdezt.jpg"
+}, {
+    "name":   "雄安图书馆",
+    "QRcode": "img/subsidiaries/7.xatsg.jpg"
+}, {
+    "name":   "顺义天竺",
+    "QRcode": "img/subsidiaries/8.sytz.jpg"
+}, {
+    "name":   "顺义樱花园",
+    "QRcode": "img/subsidiaries/9.syyhy.jpg"
+}, {
+    "name":   "北京东站货场",
+    "QRcode": "img/subsidiaries/10.bjdzhc.jpg"
+}, {
+    "name":   "德阳群众文化中心",
+    "QRcode": "img/subsidiaries/11.dyqzwhzx.jpg"
+}, {
+    "name":   "眉山加州智慧城",
+    "QRcode": "img/subsidiaries/12.msjzzhc.jpg"
+}, {
+    "name":   "青岛中法客厅",
+    "QRcode": "img/subsidiaries/13.qdzfkt.jpg"
+}
+]
